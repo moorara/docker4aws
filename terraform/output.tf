c5e3172c4951efe60a3018e5f29a192a48ccc135
Milad Irannejad <moorara@gmail.com>
2018-04-20 16:03:53 -0400

    Update Docker version to 18.03

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,15 +1,27 @@
+output "managers" {
+  value = "${lookup(var.config, "${var.size}.manager.count")}"
+}
+
+output "workers" {
+  value = "${lookup(var.config, "${var.size}.worker.count")}"
+}
+
+output "whitelist" {
+  value = "${var.whitelist}"
+}
+
 output "vpc_cidr" {
   value = "${aws_vpc.d4aws.cidr_block}"
 }
 
 output "subnet_cidr" {
-  value = [ "${aws_subnet.d4aws.*.cidr_block}" ]
+  value = ["${aws_subnet.d4aws.*.cidr_block}"]
 }
 
 output "subnet_zone" {
-  value = [ "${join(", ", aws_subnet.d4aws.*.availability_zone)}" ]
+  value = ["${join(", ", aws_subnet.d4aws.*.availability_zone)}"]
 }
 
 output "cloudformation" {
-  value = [ "${aws_cloudformation_stack.d4aws.*.outputs}" ]
+  value = ["${aws_cloudformation_stack.d4aws.*.outputs}"]
 }
